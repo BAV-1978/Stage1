ac8dc6e1dc999a5a7f5249ad0968429185792924
BAV-1978 <new_gr@mail.ru>
2024-05-17 02:24:23 +0500

    Add files via upload

---
+++ 2.py
@@ -0,0 +1,35 @@
+number = int(input("введите  число:"))
+number_str = str(number)
+length = len(number_str)
+print("длина равна:",length)
+
+# новое задание
+
+a = "Вторник"
+b = "Понедельник"
+print(b,",",a)
+
+#сумма  и  разность
+a =(15)
+b =(10)
+c = a-b
+d = a+b
+print(str(c) , str(d))
+
+# средне о=арифметическое
+a =(20)
+b =(30)
+c =(70)
+srednee = (a+b+c) /3
+srednee_str =str(srednee)
+print("среднее  арифметическое  равно :" , srednee_str)
+
+# сложная  формула    однако.
+a = (1)
+b = (2)
+c = (4)
+f = (a*b)+(a*c)
+result = f ** 3 / 2
+result_str = str(result)
+print("Результат:", result_str)
+
