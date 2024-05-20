f5444c6d6e756eaad881000b816040cf3f44bfe8
BAV-1978 <new_gr@mail.ru>
2024-05-21 00:08:52 +0500

    Add files via upload

--- homework2.py
+++ homework2.py
@@ -1,8 +1,24 @@
 DZ =12
 hour = 1.5
-Kurs = 'Pyton'
+Kurs = 'Python'
 time_DZ = 1.5/12
 print( str('Курс:')+ ' ' +str(Kurs) + ', ' + str('всего задач 12:') + ', ' + str('затрачено часов:')  +str(hour) + ', '+ str('среднее время выполнения:') + str(time_DZ) +' ' + str('часа'))
+
+# динамическая типизация
+
+name = 'Алексей'
+print('Name:', name)
+age = int(45)
+print('Age:', age)
+age = int(45)+int(1)
+print('New age:', age)
+is_student = True
+print('Student:', is_student)
+
+
+
+
+
 
 
 
