a537363d63b2b2c2ec5689503f95548db9460b78
BAV-1978 <new_gr@mail.ru>
2024-05-21 02:22:48 +0500

    Add files via upload

---
+++ homework3.py
@@ -0,0 +1,11 @@
+my_string = input("Ведите начало стихотворения: ")
+print(my_string)
+print('Длина введенного текста: ', len(my_string))
+print(my_string .upper())
+print(my_string .lower())
+print(my_string .replace(' ', ''))
+print(my_string[0])
+print(my_string[-1])
+
+
+
